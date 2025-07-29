e6a56a93c3209154e9840ea6cda653fed1d65aa7
Diana Budova <budovadiana@gmail.com>
2025-07-29 14:15:09 +0000

    Stripe: Done

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,7 +2,9 @@ import FlashLayout from "./FlashLayout";
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-stone-100 pt-6 sm:justify-center sm:pt-0">
+        <div className="flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0 overflow-hidden bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+            <div className="absolute left-0 right-0 -z-10 m-auto h-[450px] w-[850px] rounded-full bg-indigo-300 opacity-20 blur-[100px]"></div>
+
             <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-md">
                 <FlashLayout>
                     {children}
--- resources/js/Pages/Plan/Plans.jsx
+++ resources/js/Pages/Plan/Plans.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Card from "@/Components/Plan/Card";
 import Toggle from "@/Components/Toggle";
 import FlashLayout from "@/Layouts/FlashLayout";
+import SecondaryButton from "@/Components/SecondaryButton";
+import { router } from "@inertiajs/react";
+import { StepBack } from "lucide-react";
 
 export default function Plans({ plans }) {
     const [isYearly, setIsYearly] = useState(true);
@@ -9,9 +12,21 @@ export default function Plans({ plans }) {
     const interval = isYearly ? "yearly" : "monthly";
     const filteredPlans = plans.filter((plan) => plan.interval === interval);
 
+    const handleBack = () => {
+        router.get('/dashboard');
+    };
+
     return (
         <div className="relative min-h-screen w-full overflow-hidden bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
             <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[450px] w-[850px] rounded-full bg-indigo-300 opacity-20 blur-[100px]"></div>
+
+            <SecondaryButton
+                className="absolute top-4 left-4"
+                onClick={handleBack}
+            >
+                <StepBack className="mr-2 h-4 w-4" /> <span>Go Back</span>
+            </SecondaryButton>
+
             <section className="relative z-10 mx-auto max-w-3xl p-8">
                 <FlashLayout>
                     <h1 className="mb-8 text-center text-4xl font-bold">Plans & Pricing</h1>
